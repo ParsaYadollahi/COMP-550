a6ff6c4f2af67a90ee6ce19d7f8e02272063a3a6
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2020-11-18 11:51:27 -0500

    Simplified lesk count done

--- A2/A2_starter_code/main.py
+++ A2/A2_starter_code/main.py
@@ -6,6 +6,7 @@ dev_instances, test_instances, dev_key, test_key = load_data()
 
 most_freq_baseline_count = 0
 lesk_count = 0
+simplified_lest_count = 0
 
 # run most frequent baseline and lesks algorithm
 for idx, wsd_instance in test_instances.items():
@@ -14,23 +15,19 @@ for idx, wsd_instance in test_instances.items():
     w = wsd(wsd_instance)
 
     most_frequent_baseline_sense_pred = w.most_frequent_sense_baseline()
-    if (w.computeOverlap(sense, most_frequent_baseline_sense_pred) > 0):
+    if w.computeOverlap(sense, most_frequent_baseline_sense_pred) > 0:
         most_freq_baseline_count = most_freq_baseline_count + 1
 
     lesk_sense_pred = w.lesk()
-    if (w.computeOverlap(sense, lesk_sense_pred) > 0):
+    if w.computeOverlap(sense, lesk_sense_pred) > 0:
         lesk_count = lesk_count + 1
 
-print(most_freq_baseline_count/len(test_instances))
-print(lesk_count/len(test_instances))
+    simplified_lesk_sense_results, max_overlap = w.simplified_lesk()
+    if (max_overlap > 0):
+        simplified_lest_count += 1
 
 
-#     most_freq_baseline_sense_pred = wsd.most_frequent_baseline()
-#     most_freq_baseline_count += 1 if len(
-#         sense.intersection(most_freq_baseline_sense_pred)) > 0 else 0
-
-#     lesk_sense_pred = wsd.run_lesks()
-#     lesk_count += 1 if len(sense.intersection(lesk_sense_pred)) > 0 else 0
-
-# print(most_freq_baseline_count/len(test_instances))
-# print(lesk_count/len(test_instances))
+print("Most frequent baseline count  = ",
+      most_freq_baseline_count / len(test_instances))
+print("Lesk count = ", lesk_count / len(test_instances))
+print("Simplified Lesk count = ", simplified_lest_count / len(test_instances))
--- A2/A2_starter_code/wsd.py
+++ A2/A2_starter_code/wsd.py
@@ -1,6 +1,7 @@
 # NLTK
 from os import getsid
 from re import match
+from types import SimpleNamespace
 from typing import overload
 from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
@@ -13,7 +14,7 @@ import re
 import string
 
 DATAENCODING = "utf-8"
-STOPWORDS = set(stopwords.words('english'))
+STOPWORDS = set(stopwords.words("english"))
 LEMMATIZER = WordNetLemmatizer()
 
 
@@ -29,8 +30,9 @@ class wsd:
         lemma = wsd_instance.lemma.decode(DATAENCODING)
         lemma = lemma.lower()
 
-        preprocessed_context = ' '.join(
-            [c.decode(DATAENCODING) for c in wsd_instance.context])
+        preprocessed_context = " ".join(
+            [c.decode(DATAENCODING) for c in wsd_instance.context]
+        )
 
         text = preprocessed_context.lower()
         text = re.sub("\\W", " ", text)
@@ -38,7 +40,9 @@ class wsd:
 
         # Remove stopwords and punctuation
         basic_preprocess_text = [
-            word for word in text if word not in STOPWORDS and word not in string.punctuation
+            word
+            for word in text
+            if word not in STOPWORDS and word not in string.punctuation
         ]
 
         lemmatized_text = []
@@ -46,11 +50,10 @@ class wsd:
         # tag with pos to get accurate results from lemmatizer
         for word, tag in pos_tag(basic_preprocess_text):
             wntag = tag[0].lower()
-            wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
+            wntag = wntag if wntag in ["a", "r", "n", "v"] else None
 
             if wntag:
-                lemmatized_text.append(
-                    LEMMATIZER.lemmatize(word, wntag))
+                lemmatized_text.append(LEMMATIZER.lemmatize(word, wntag))
             else:
                 lemmatized_text.append(word)
 
@@ -81,10 +84,7 @@ class wsd:
 
         return pred
 
-
-# Simplified Lesk
     # method for counting overlap
-
 
     def computeOverlap(self, signature, context):
         return len(context.intersection(signature))
@@ -94,29 +94,26 @@ class wsd:
         return [word for word in tokens if not word in STOPWORDS]
 
     def getSignature(self, sense):
-
         wordSet = set(self.filterText(sense.definition()))
 
         for example in sense.examples():
             wordSet = wordSet.union(set(self.filterText(example)))
-
         return wordSet
 
     def simplified_lesk(self):  # returns best sense of word
         # Implementation based on Wikipedias simplified lesk Algo
         lemma = self.lemma
-        context = self.preprocessed_context
+        context = set(self.preprocessed_context)
         maxOverlap = 0
         senses = wordnet.synsets(lemma)
         bestSense = senses[0]
-        context = self.preprocessed_context
 
-        for sense in senses:
-            signature = self.getSignature(sense)
-            overlap = self.computeOverlap(signature=signature, context=context)
+        for matchingSense in senses:
+            signature = self.getSignature(matchingSense)
+            overlap = self.computeOverlap(signature, context)
 
             if overlap > maxOverlap:
                 maxOverlap = overlap
-                bestSense = sense
+                bestSense = matchingSense
 
-        return bestSense
+        return bestSense, maxOverlap
